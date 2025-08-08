f654de8e15539b408992608e7c83bf22a39b35a6
zhoupengzu@kanzhun.com <zhoupengzu@kanzhun.com>
2025-08-08 10:45:09 +0800

    update

---
+++ PZObjcSwiftUIHome/Classes/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  PZObjectiveSwiftUI
+//
+//  Created by admin on 2025/8/7.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PZObjcSwiftUIHome/Classes/PZObjcSwiftUIHomeGlassEffect.swift
@@ -0,0 +1,18 @@
+//
+//  PZObjcSwiftUIHomeGlassEffect.swift
+//  PZObjcSwiftUIHome
+//
+//  Created by admin on 2025/8/8.
+//
+
+import SwiftUI
+
+struct PZObjcSwiftUIHomeGlassEffect: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PZObjcSwiftUIHomeGlassEffect()
+}
